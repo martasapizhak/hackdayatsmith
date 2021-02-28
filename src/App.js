09df83fa6d15175df554052962d0110d825a8bdb
AnandaMontoly <amontoly@smith.edu>
2021-02-28 15:48:48 -0500

    pweese be nice

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import Accordian from "./ControlledAccordian.js"
 import Tracks from "./tracks.js";
+import Link from "@material-ui/core/Link";
 import {Helmet} from "react-helmet";
 
 
@@ -29,8 +30,7 @@ function App(){
               <h1>Our Sponsors</h1>
               HackSmith wouldn't be possible without the generosity of Audible, the Conway Center for Innovation, The Design Thinking Initiative, Google, and MassMutual.
               <h2>Design Thinking Initiative Support</h2>
-              If you're interested in checking out physical materials (including Arduino Kits) from the Design Thinking Initiative, please sign up to do so <a href="https://www.librarycat.org/lib/designthinking">here!</a>
-              There will be a physical drop-off point for those who are interested.
+              If you're interested in checking out physical materials (including Arduino Kits) from the Design Thinking Initiative, please sign up to do so <Link href="https://www.librarycat.org/lib/designthinking" style={{color:"#e6cb49"}}>here!</Link> There will be a physical drop-off point for those who are interested.
 
 
               <h1>Tracks</h1>
